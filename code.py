851df80122089a7e374064cf222714a94145e17b
Parthiv Khaund <parthivkhaund@gmail.com>
2020-05-11 22:20:12 -0400

    added tips for productivity

--- code.py
+++ code.py
@@ -20,11 +20,22 @@ def log_asker():
 
 
 # asks if feel productive or not 
+# integrated tips with productivity checker 
 
 def productive_checker():
 	answer = "sample"
 	while (answer!= "yes" and answer!= "no"):
 		answer = input("were you productivity? :")
+		if answer == "no":
+			tips = input("do you wish to review productivity tips ? :")
+			if tips == "yes":
+				print("DO Just Start\n")	
+				print("DO set a hard deadline for tasks\n")							
+				print("DONT think its a small task will do it later\n")
+				print("DO if cravings , breathe and be in control\n")
+				print("DO list down all tasks (DONT be intimidated)\n")
+				print("DO Pomodoro Technique\n")
+				print("DO if other stuff comed to mind add to distraction list\n")
 	productive_hours.append(answer)
 
 
@@ -43,6 +54,9 @@ def running_for_hours():
 		print("please give a number")
 
 # gives them tips for productivity 
+
+
+
 # gives an hourly (csv) log of user inputs
 
 #outputs
